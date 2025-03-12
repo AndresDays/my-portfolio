2556dc306cf671a6d4188007a91569f414663e9f
AndresDays <119088908+AndresDays@users.noreply.github.com>
2025-03-12 17:06:44 -0600

    added dark mode.js

--- script.js
+++ script.js
@@ -25,3 +25,50 @@ function showPopup(bool) {
 		document.getElementById('popup').style.visibility = 'hidden'
 	}
 }
+
+let esNegro = false;
+
+  document.getElementById("changeColor").addEventListener("click", function(event) {
+    event.preventDefault(); 
+    if (esNegro) {
+      document.body.style.backgroundColor = "white";
+      document.body.style.color = "black";
+	  document.getElementById('home').style.backgroundColor = '#308af1';
+	  document.getElementById('changeColor').style.backgroundColor = '';
+	  document.getElementById('darkTitle').style.color = 'black';
+	  let anchors = document.querySelectorAll(".certification-title");
+	anchors.forEach(function(anchor) {
+		anchor.style.color = "black";
+	  });
+	let titles = document.querySelectorAll(".project-title");
+	titles.forEach(function(title) {
+		title.style.color = "black";
+	  });
+	let recommendations = document.querySelectorAll(".recommendation");
+	recommendations.forEach(function(recommendation) {
+		recommendation.style.color = "black";
+		recommendation.style.backgroundColor = 'white';
+	  });
+    } else {
+      document.body.style.backgroundColor = 'hsla(0, 0%, 10%)';
+      document.body.style.color = "white";
+	  document.getElementById('home').style.backgroundColor = 'hsla(0, 0%, 12%)';
+	document.getElementById('changeColor').style.backgroundColor = 'hsla(0, 0%, 25%)';
+	document.getElementById('darkTitle').style.color = 'white';
+	let anchors = document.querySelectorAll(".certification-title");
+	anchors.forEach(function(anchor) {
+		anchor.style.color = "white";
+	  });
+	let titles = document.querySelectorAll(".project-title");
+	titles.forEach(function(title) {
+		title.style.color = "white";
+	  });
+	let recommendations = document.querySelectorAll(".recommendation");
+	recommendations.forEach(function(recommendation) {
+		recommendation.style.color = "white";
+		recommendation.style.backgroundColor = 'hsla(0, 0%, 25%)';
+	  });
+    }
+	
+    esNegro = !esNegro;
+  });
